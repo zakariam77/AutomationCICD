1346e5087347dd0e8c8493c0eb63ccbda9d4f4e9
zakariam77 <zakariamoujahid90@gmail.com>
2026-04-22 18:23:41 +0100

    clean

--- src/test/java/tests/submitOrder.java
+++ src/test/java/tests/submitOrder.java
@@ -43,7 +43,7 @@ public class submitOrder extends BaseTest{
        inventory.addProductToCart(pName);
        Cart cart = inventory.goToCart();
        String productInCart = cart.verifyProductInCart(pName);
-       softAssert.assertEquals(productInCart, "fake name");
+       softAssert.assertEquals(productInCart, pName);
        softAssert.assertAll();
     }
 
@@ -52,7 +52,7 @@ public class submitOrder extends BaseTest{
         SoftAssert softAssert = new SoftAssert();
         LandingPage landingPage = new LandingPage(DriverManager.getDriver());
         landingPage.loginApp(input.get("name"), input.get("last"));
-        softAssert.assertEquals(landingPage.getErrorMsg(), "cEpic sadface: Username and password do not match any user in this service");
+        softAssert.assertEquals(landingPage.getErrorMsg(), "Epic sadface: Username and password do not match any user in this service");
         softAssert.assertAll();
     }
 
