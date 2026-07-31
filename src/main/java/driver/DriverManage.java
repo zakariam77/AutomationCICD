42080e3bec870e27ee2e7a8cfb8db8a8a173eb68
zakariam77 <zakariamoujahid90@gmail.com>
2026-07-31 22:26:10 +0100

    f

--- src/main/java/driver/DriverManage.java
+++ src/main/java/driver/DriverManage.java
@@ -1,9 +1,13 @@
 package driver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 
 public class DriverManage {
 
+    private static final Logger logger = LogManager.getLogger(DriverManage.class);
     private static final ThreadLocal<WebDriver> DRIVER_THREAD_LOCAL = new ThreadLocal<>();
 
     public static void setDriverThreadLocal(WebDriver instanceDriver){
@@ -13,9 +17,14 @@ public class DriverManage {
         return DRIVER_THREAD_LOCAL.get();
     }
     public static void removeDriver(){
+
         if (DRIVER_THREAD_LOCAL.get() != null) {
-            DRIVER_THREAD_LOCAL.get().quit();
-            DRIVER_THREAD_LOCAL.remove();
+            try {
+                DRIVER_THREAD_LOCAL.get().quit();
+                DRIVER_THREAD_LOCAL.remove();
+            } catch (UnreachableBrowserException e) {
+                logger.fatal("Browser was already dead when attempting quit: {}", e.getMessage());
+            }
         }
     }
 }
