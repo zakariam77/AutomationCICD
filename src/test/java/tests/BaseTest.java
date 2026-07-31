77175eab2f0c73b318ca65b79048788054cd8e1e
zakariam77 <zakariamoujahid90@gmail.com>
2026-07-31 20:47:13 +0100

    refactoring

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -13,7 +13,7 @@ public class BaseTest {
     @BeforeMethod(alwaysRun = true)
     public void launchApplication(){
 
-        WebDriver driver  =  new ChromeDriver(); // DriverFactory.setUp();
+        WebDriver driver  =  DriverFactory.setUp();
         DriverManage.setDriverThreadLocal(driver);
         driver.get(ConfigReader.getProperty("url"));
     }
