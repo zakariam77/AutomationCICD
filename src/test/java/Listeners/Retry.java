a1b6d135aaece358eb0857a3b313664fbee4431e
zakariam77 <zakariamoujahid90@gmail.com>
2026-08-01 23:52:40 +0100

    testing logging step

--- src/test/java/Listeners/Retry.java
+++ src/test/java/Listeners/Retry.java
@@ -1,15 +1,21 @@
 package Listeners;
 
+import io.qameta.allure.Step;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class Retry implements IRetryAnalyzer {
+    private static final Logger logger = LogManager.getLogger(Retry.class);
     private final int maxTry = 1;
     private int count = 0;
+    @Step("Retrying test")
     @Override
     public boolean retry(ITestResult iTestResult) {
         if(count<maxTry){
             count++;
+            logger.info("Test failed, Retrying number: {}", count);
             return true;
         }
             return false;
